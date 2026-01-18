82da8d12b75d502d878179051e9b4a4ab2924b33
nabilfaturr <108581394+nabilfaturr@users.noreply.github.com>
2026-01-19 00:14:01 +0700

    trigger ci/cd

--- scripts.sh
+++ scripts.sh
@@ -18,3 +18,19 @@ kubectl rollout restart deployment uas-todo-frontend
 docker build -t uas-backend:latest ./backend                
 docker build -t uas-frontend:latest ./frontend              
 docker build -t uas-db:latest ./db 
+
+minikube addons enable metrics-server
+
+kubectl get svc
+
+kubectl port-forward svc/uas-todo-frontend 8080:80
+
+# Terminal Run load generator
+kubectl run -i --tty load-generator \
+  --rm \
+  --image=busybox \
+  --restart=Never -- \
+  /bin/sh -c "while sleep 0.01; do wget -q -O- http://uas-todo-backend:3000/health; done"
+
+
+ kubectl port-forward svc/uas-todo-backend 3001:3000
